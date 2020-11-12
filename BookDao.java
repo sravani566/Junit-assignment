cbb06d990e124928a33f6e363ffb32cc00a0c641
sravani566 <69153232+sravani566@users.noreply.github.com>
2020-11-12 09:07:17 +0530

    Add files via upload

---
+++ BookDao.java
@@ -0,0 +1,12 @@
+package bookApplication;
+
+import java.util.List;
+
+public interface BookDao {
+	List<Book> getAllBooks();
+	Book updateBook( int id,int page );
+	Book deleteBook( int id );
+    void addBook(Book book );
+
+}
+
